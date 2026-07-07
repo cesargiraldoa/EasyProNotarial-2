7f01310458168022fc36bd8c2a2dc0177206f76e
cesargiraldoa <cesargiraldopersonal@gmail.com>
2026-07-07 13:41:55 -0500

    chore(frontend): load dashboard brand stylesheet

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./brand-name.css";
 import { BrandProvider } from "@/components/ui/brand-provider";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
